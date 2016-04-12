ad43b87ac4bd1bde92a8cf2fc013d6ca977fa3ff
ssarcandy <ssarcandy@gmail.com>
2016-04-12 12:28:18 +0800

    template of restaurant, dish api

---
+++ routes/api/dish.js
@@ -0,0 +1,23 @@
+var Dish = require('../../models').Dish;
+var Sequelize = require('sequelize');
+var Promise = require('bluebird');
+var _ = require('lodash');
+
+
+
+exports.create = function(req, res) {
+
+};
+
+exports.destory = function(req, res) {
+
+};
+
+exports.update = function(req, res) {
+
+};
+
+// like or dislike the dish
+exports.like = function(req, res) {
+
+};
---
+++ routes/api/restaurant.js
@@ -0,0 +1,23 @@
+var Restaurant = require('../../models').Restaurant;
+var Sequelize = require('sequelize');
+var Promise = require('bluebird');
+var _ = require('lodash');
+
+
+
+exports.create = function(req, res) {
+
+};
+
+exports.destory = function(req, res) {
+
+};
+
+exports.update = function(req, res) {
+
+};
+
+// list of Restaurant's dishes
+exports.dishesList = function(req, res) {
+
+};
