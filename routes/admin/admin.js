03bec4cb1ebaf16459a084d6c4792e3b6e537648
ssarcandy <ssarcandy@gmail.com>
2016-04-18 15:44:29 +0800

    admin create restaurant

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -154,3 +154,46 @@ exports.dishCreate = function(req, res) {
         });
 };
 
+/**
+ * @api {post} /admin/restaurant Create Restaurant
+ * @apiName admin.restaurantCreate
+ * @apiGroup admin 
+ * @apiPermission Admin
+ *
+ * @apiParam {string} name restaurant name
+ * @apiParam {float} lat latitude of restaurant 
+ * @apiParam {float} lon longtitude of restaurant 
+ * @apiParam {string} location address of restaurant 
+ * @apiParam {string} [description] description of restaurant
+ * @apiParam {string} [photo] restaurant's photo filename.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 201 OK
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 500 error
+ */
+exports.restaurantCreate = function(req, res) {
+    let newRestaurant = {
+        name: req.body.name,
+        description: req.body.description,
+        photo: req.body.photo,
+        location: req.body.location,
+        lat: parseFloat(req.body.lat),
+        lon: parseFloat(req.body.lon)
+    };
+
+    if (!newRestaurant.name || !newRestaurant.location || !newRestaurant.lat || !newRestaurant.lon) {
+        return res.status(400).json({
+            msg: 'missing value.'
+        });
+    }
+
+    Restaurant
+        .create(newRestaurant)
+        .then(()=> { res.status(201).send(); })
+        .catch((err)=> {
+            console.error(err);
+            res.status(500).send();
+        });
+};
--- routes/admin/index.js
+++ routes/admin/index.js
@@ -9,5 +9,6 @@ app.get('/memberlist', middlewares.checkAdmin, admin.memberList);
 app.get('/restaurantlist', middlewares.checkAdmin, admin.restaurantList);
 
 app.post('/dish', middlewares.checkAdmin, admin.dishCreate);
+app.post('/restaurant', middlewares.checkAdmin, admin.restaurantCreate);
 
 module.exports = app;
